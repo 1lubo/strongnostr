a9764a839137af3172e6d4eeec325dfc4c83784b
1lubo <1lubo@pm.me>
2025-06-13 10:00:37 +0200

    Initial commit

---
+++ src/main/java/com/_lubo/strongnostr/StrongnostrApplication.java
@@ -0,0 +1,13 @@
+package com._lubo.strongnostr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrongnostrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrongnostrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_lubo/strongnostr/StrongnostrApplicationTests.java
@@ -0,0 +1,13 @@
+package com._lubo.strongnostr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrongnostrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
