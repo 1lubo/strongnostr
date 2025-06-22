1b7552aafb55cf055147947797bb6be39708341c
1lubo <1lubo@pm.me>
2025-06-22 09:13:36 +0200

    Add WorkoutRepository, ExerciseRepository

---
+++ src/main/java/com/onelubo/strongnostr/repository/WorkoutRepository.java
@@ -0,0 +1,7 @@
+package com.onelubo.strongnostr.repository;
+
+import com.onelubo.strongnostr.model.Workout;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface WorkoutRepository extends MongoRepository<Workout, String> {
+}
