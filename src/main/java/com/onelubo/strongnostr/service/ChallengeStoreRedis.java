a202a331f2a8a0dd919663c2897604685b50aa8c
1lubo <1lubo@pm.me>
2025-06-29 05:58:51 +0200

    Add ChallengeStore

---
+++ src/main/java/com/onelubo/strongnostr/service/ChallengeStoreRedis.java
@@ -0,0 +1,60 @@
+package com.onelubo.strongnostr.service;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Profile("redis")
+@Component
+public class ChallengeStoreRedis implements ChallengeStore {
+
+    private final RedisTemplate<String, StoredChallenge> redisTemplate;
+    private final ScheduledExecutorService cleanupExecutor = Executors.newSingleThreadScheduledExecutor();
+
+    public ChallengeStoreRedis(RedisTemplate<String, StoredChallenge> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @PostConstruct
+    public void init() {
+        cleanupExecutor.scheduleAtFixedRate(this::cleanupExpired, 1, 1, TimeUnit.MINUTES);
+    }
+    @Override
+    public void storeChallenge(String challengeId, String challenge, long expiresAt) {
+        StoredChallenge stored = new StoredChallenge(challenge, expiresAt, false);
+        long ttl = expiresAt - System.currentTimeMillis();
+        redisTemplate.opsForValue().set(challengeId, stored, ttl, TimeUnit.MILLISECONDS);
+
+    }
+
+    @Override
+    public Optional<StoredChallenge> getChallenge(String challengeId) {
+        return Optional.ofNullable(redisTemplate.opsForValue().get(challengeId));
+    }
+
+    @Override
+    public boolean markChallengeAsUsed(String challengeId) {
+        StoredChallenge challenge = redisTemplate.opsForValue().get(challengeId);
+        if (challenge != null && !challenge.used() && challenge.expiresAt() > System.currentTimeMillis()) {
+            redisTemplate.opsForValue().set(challengeId, challenge.markUsed());
+            return  true;
+        }
+        return false;
+    }
+
+    @Override
+    public void cleanupExpired() {
+        // Redis does not require manual cleanup of expired keys, as it handles this automatically.
+    }
+
+    @Override
+    public void shutdown() {
+        // No-op for Redis, as it handles cleanup automatically
+    }
+}
