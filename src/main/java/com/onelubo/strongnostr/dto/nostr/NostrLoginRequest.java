39e99052fd8ff53872f85d9f00272c1037444b25
1lubo <1lubo@pm.me>
2025-07-02 04:42:42 +0200

    Remove unused dto

--- src/main/java/com/onelubo/strongnostr/dto/nostr/NostrLoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.onelubo.strongnostr.dto.nostr;
-
-public record NostrLoginRequest(
-        String challengeId,
-        String npub,
-        String signature,
-        String challenge
-) {
-}
